6a15860a19a6d3bb9d310434410688e9f164e935
Notdog <xwy_02@163.com>
2022-06-09 16:30:52 +0800

    prodAndCons v2

--- prodcons/main.cpp
+++ prodcons/main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 mutex mtx;
 condition_variable cv;
+condition_variable fullCv;
+condition_variable notEmptyCv;
 const int maxDequeSize = 30;
 // 共享资源（工厂）
 struct item {
@@ -17,25 +19,41 @@ struct item {
 item* items = new item();
 
 void produce() {
-    unique_lock<mutex> lock(mtx);
-    if (items->size >= maxDequeSize) {
-        cv.notify_all();
-        return;
+    while (1) {    
+        unique_lock<mutex> lock(mtx);
+        // if (items->size >= maxDequeSize) {
+        //     cv.notify_all();
+        //     return;
+        // }
+
+        // items->size < maxDequeSize 继续执行； 否则（items->size >= maxDequeSize），被wait住
+        fullCv.wait(lock, []()
+                    { return items->size < maxDequeSize; });
+        // 生产
+        items->dq.push_back(111);
+        items->size++;
+        // cv.notify_all();
+        // 通知消费者线程消费
+        notEmptyCv.notify_all();
     }
-    // 生产
-    items->dq.push_back(111);
-    items->size++;
-    cv.notify_all();
 }
 void consume() {
-    unique_lock<mutex> lock(mtx);
-    while (items->size == 0) {
-        cv.wait(lock);
-        // return;
+    while (1) {
+        unique_lock<mutex> lock(mtx);
+        // while (items->size == 0) {
+        //     cv.wait(lock);
+        //     // return;
+        // }
+
+        // items->size > 0 继续执行； 否则(items->size <= 0)， 被wait住
+        notEmptyCv.wait(lock, []()
+                        { return items->size > 0; });
+        // 消费
+        items->dq.pop_front();
+        items->size--;
+        // 通知生产者线程生产
+        fullCv.notify_all();
     }
-    // 消费
-    items->dq.pop_front();
-    items->size--;
 }
 int main() {
 
